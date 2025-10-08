0179b693ad9b3ea4d674899b69d4e89ca1e6ba26
ColeJNewsome <colenewsome355@yahoo.com>
2025-10-07 22:52:53 -0400

    Added functionality for demo calculation  button

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ const observer = new IntersectionObserver(entries => {
 appearingElements.forEach(element => {
     observer.observe(element);
 });
+
+CalculateResults = () => {
+    let tds = document.getElementById("TDS").value;
+    let pH = document.getElementById("pH").value;
+    let temp = document.getElementById("Temp").value;
+    let Bacteria = document.getElementById("Bacteria").value;
+    if (bacteria == ("EColi" | "Other") | tds > 1000 | temp > 120 | pH < 6 | pH > 9) {
+        document.getElementById("result").value = "DO NOT DRINK";
+        return;
+    }
+    else if (tds > 500 | pH < 6.5 | pH > 8.5 | temp > 100) {
+        document.getElementById("result").value = "Potentially Unsafe or Unpleasant Taste";
+        return;
+    }
+    else if (tds < 500 & tds > 150){
+        document.getElementById("result").value = "Likely Safe to Drink";
+        return;
+    }
+    else {
+        document.getElementById("result").value = "Safe to Drink";
+        return;
+    }
+}
+    
